49051ac0fd574298bc2f80178dbfc9d848fe454e
selemondev <selemondev19@gmail.com>
2022-07-15 06:37:44 +0300

    auth functionality

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,17 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const colors = require("colors");
 const PORT = process.env.PORT || 5000;
+const { errorMiddleware } = require("./middleware/errorMiddleware");
+const authRoute = require("./routes/authRoutes");
+const connectDB = require("./config/db");
 const app = express();
+connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-
-app.use(PORT, () => {
+app.use("/api/auth", authRoute)
+app.use(errorMiddleware)
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
 
